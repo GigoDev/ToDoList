1a6cee84a8389e949e389a4f382b7fe579758d97
Gigo Dev <guyrick21@gmail.com>
2024-10-17 17:41:10 +0300

    Use relative path to import

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom' 
+import { HashRouter } from 'react-router-dom'
 import { RootCmp } from './RootCmp.jsx'
 import './assets/styles/main.scss'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-	<BrowserRouter basename="/"> 
+	<HashRouter>
 		<RootCmp />
-	</BrowserRouter>
+	</HashRouter>
 )
